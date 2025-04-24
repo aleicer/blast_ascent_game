689a575205a783e293a0fe979754b2ff7e31d8f5
Aleicer <aleicer.vesga@gmail.com>
2025-04-23 21:06:35 -0500

    Agregue botones de derecha e izquierda, pendiente funcionalidad

--- Assets/Scripts/PlayerScript.cs
+++ Assets/Scripts/PlayerScript.cs
@@ -2,15 +2,40 @@ using UnityEngine;
 
 public class PlayerScript : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    [SerializeField] private float movementSpeed;
+    [SerializeField] private Rigidbody2D rigidBody2d;
+    private float horizontalMovement;
+    private Vector3 speed = Vector3.zero;
+    private InputMovement inputActions;
+
+    private void Awake()
     {
-        
+        inputActions = new InputMovement();
     }
 
-    // Update is called once per frame
-    void Update()
+    private void OnEnable()
     {
-        
+        inputActions.Enable();
+    }
+
+    private void OnDisable()
+    {
+        inputActions.Disable();
+    }
+
+    private void Update()
+    {
+        horizontalMovement = inputActions.Movement.Horizontal.ReadValue<float>() * movementSpeed;
+    }
+
+    private void FixedUpdate()
+    {
+        Move(horizontalMovement * Time.fixedDeltaTime);
+    }
+
+    private void Move(float move)
+    {
+        Vector3 targetSpeed = new Vector2(move, rigidBody2d.linearVelocity.y);
+        rigidBody2d.linearVelocity = Vector3.SmoothDamp(rigidBody2d.linearVelocity, targetSpeed, ref speed, 0f);
     }
 }
